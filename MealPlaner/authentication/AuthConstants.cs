297531c299b7fbcce3779a033d790c0e8bc49f17
Martin Zagorc <martin.zagorc42@gmail.com>
2024-11-06 16:24:04 +0100

    delette api key auth

--- MealPlaner/authentication/AuthConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MealPlaner.authentication
-{
-    public class AuthConstants
-    {
-        public const string ApiKeySectionName = "Authentication:ApiKey";
-        public const string ApiKeyheaderName = "Api-Key";
-
-    }
-}
